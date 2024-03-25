8ccbbf310f0acb3465996bf280fad44f3285089b
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2024-03-25 18:44:54 +0200

    add favorite list style

---
+++ src/components/FavoritesList/FavoritesList.styled.jsx
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+
+export const FavoriteList = styled.ul`
+  margin: 40px 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 40px;
+`;
