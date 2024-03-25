95194576c96aa772232fa765eb633727a482c758
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2024-03-25 12:34:04 +0200

    add redux helpers/get total reducers

--- src/redux/catalog/catalogSlice.js
+++ src/redux/catalog/catalogSlice.js
@@ -1,8 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { bookForm, getAllCatalog } from "./catalogOperations";
+import { bookForm, getAllCatalog, getTotal } from "./catalogOperations";
+import { handlePending, handleRejected } from "../../redux/helpers";
 
 const initialState = {
     catalog: [],
+    total: 13,
     isLoading: false,
 }
 
@@ -11,25 +13,23 @@ const catalogSlice = createSlice({
     initialState,
     extraReducers: builder => {
         builder
-            .addCase(getAllCatalog.pending, (state) => {
-                state.isLoading = true;
-            })
+            .addCase(getAllCatalog.pending, handlePending)
             .addCase(getAllCatalog.fulfilled, (state, { payload }) => {
                 state.isLoading = false;
                 state.catalog = [...payload];
             })
-            .addCase(getAllCatalog.rejected, (state) => {
-                state.isLoading = false;
-            })
-            .addCase(bookForm.pending, (state) => {
-                state.isLoading = true;
-            })
+            .addCase(getAllCatalog.rejected, handleRejected)
+            .addCase(bookForm.pending, handlePending)
             .addCase(bookForm.fulfilled, (state) => {
                 state.isLoading = false;
             })
-            .addCase(bookForm.rejected, (state) => {
+            .addCase(bookForm.rejected, handleRejected)
+            .addCase(getTotal.pending, handlePending)
+            .addCase(getTotal.fulfilled, (state, { payload }) => {
+                state.total = payload.length;
                 state.isLoading = false;
             })
+            .addCase(getTotal.rejected, handleRejected)
     }
 })
 
