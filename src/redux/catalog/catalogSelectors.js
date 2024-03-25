fb3ca5ae243435442c546393c718960ed376d92f
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2024-03-25 18:43:04 +0200

    add favorite selector

--- src/redux/catalog/catalogSelectors.js
+++ src/redux/catalog/catalogSelectors.js
@@ -1,3 +1,4 @@
 export const selectAllCatalog = ({ catalog }) => catalog.catalog;
 export const selectIsLoading = ({ catalog }) => catalog.isLoading;
 export const selectTotal = ({ catalog }) => catalog.total;
+export const selectFavorites = ({ catalog }) => catalog.favorites;
