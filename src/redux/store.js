71e89d87f8746d644335023f5e5eb5dab8cd72a4
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2024-03-25 15:46:47 +0200

    add fav persist config/persist middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,33 @@
 import { configureStore } from "@reduxjs/toolkit";
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import { catalogReducer } from "./catalog/catalogSlice";
+
+const catalogPersistConfig = {
+    key: "favorites",
+    storage,
+    whitelist: ["favorites"],
+};
 
 export const store = configureStore({
     reducer: {
-        catalog: catalogReducer,
-    }
+        catalog: persistReducer(catalogPersistConfig, catalogReducer),
+    },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
 })
+
+export const persistor = persistStore(store);
