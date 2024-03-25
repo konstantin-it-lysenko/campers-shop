1f6cca840d6f83583401efe96276ac9db3e8ac78
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2024-03-25 12:35:14 +0200

    add load more pagination

--- src/components/CamperList/CamperList.jsx
+++ src/components/CamperList/CamperList.jsx
@@ -1,24 +1,42 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { getAllCatalog } from '../../redux/catalog/catalogOperations';
-import { selectAllCatalog } from '../../redux/catalog/catalogSelectors';
+import { getAllCatalog, getTotal } from '../../redux/catalog/catalogOperations';
+import {
+  selectAllCatalog,
+  selectTotal,
+} from '../../redux/catalog/catalogSelectors';
 import CatalogItem from 'components/CatalogItem';
-import { CatalogList } from './CamperList.styled';
+import { CatalogList, CatalogWrap, LoadMoreBtn } from './CamperList.styled';
+import { limit } from 'constants/vars';
 
 const CamperList = () => {
   const dispatch = useDispatch();
   const allCatalog = useSelector(selectAllCatalog);
+  const total = useSelector(selectTotal);
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
-    dispatch(getAllCatalog());
-  }, [dispatch]);
+    dispatch(getTotal);
+    dispatch(getAllCatalog(page));
+  }, [dispatch, page]);
+
+  const handleLoadMoreBtn = () => {
+    if (page * limit >= total) {
+      setPage(1);
+    }
+
+    setPage(prev => prev + 1);
+  };
 
   return (
-    <CatalogList>
-      {allCatalog.map(card => {
-        return <CatalogItem key={card._id} id={card._id} card={card} />;
-      })}
-    </CatalogList>
+    <CatalogWrap>
+      <CatalogList>
+        {allCatalog.map(card => {
+          return <CatalogItem key={card._id} id={card._id} card={card} />;
+        })}
+      </CatalogList>
+      <LoadMoreBtn onClick={handleLoadMoreBtn}>Load more</LoadMoreBtn>
+    </CatalogWrap>
   );
 };
 
