cd8e16f2805faefbf10c4f30ffd28a532cca61f9
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2024-03-25 16:25:28 +0200

    remove wrapper

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,24 +1,22 @@
-import { Wrapper } from "../../layout/SharedLayout/SharedLayout.styled";
-import { HeaderT, Link, NavList } from "./Header.styled";
+import { Wrapper } from '../../layout/SharedLayout/SharedLayout.styled';
+import { HeaderT, Link, NavList } from './Header.styled';
 
 const Header = () => {
   return (
     <HeaderT>
-      <Wrapper>
-        <nav>
-          <NavList>
-            <li>
-              <Link to="/">HOME</Link>
-            </li>
-            <li>
-              <Link to="catalog">CATALOG</Link>
-            </li>
-            <li>
-              <Link to="/favorites">FAVORITES</Link>
-            </li>
-          </NavList>
-        </nav>
-      </Wrapper>
+      <nav>
+        <NavList>
+          <li>
+            <Link to="/">HOME</Link>
+          </li>
+          <li>
+            <Link to="catalog">CATALOG</Link>
+          </li>
+          <li>
+            <Link to="/favorites">FAVORITES</Link>
+          </li>
+        </NavList>
+      </nav>
     </HeaderT>
   );
 };
